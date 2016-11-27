b1aa39fd1fd6b49a6cc1f2ad820e7f791bd47a72
zhangwei <719048757@qq.com>
2016-11-27 09:18:21 +0800

    设置支持跨域

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,14 @@ module Eshop
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Rails 5
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:any]
+      end
+    end
+
   end
 end
