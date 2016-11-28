1f3c71ccf2758d70dfeef9d421258299d8d0b03d
zhangwei <719048757@qq.com>
2016-11-28 21:22:39 +0800

    code

--- app/services/session_service.rb
+++ app/services/session_service.rb
@@ -2,7 +2,6 @@ class SessionService < BaseService
   def login(login_params)
     account = Account.find_by(mobile_number: login_params[:username])
     if account && account.authenticate(login_params[:password])
-      self.current_user = account
       CommonService.response_format(ResponseCode.COMMON.OK, account.as_json.extract!('id', 'mobile_number', 'authentication_token'))
     else
       CommonService.response_format(ResponseCode.COMMON.FAILED)
