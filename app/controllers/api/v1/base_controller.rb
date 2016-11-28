05ba64a11639656fc7a0b2d3cf302e8ee47c28cf
zhangwei <719048757@qq.com>
2016-11-29 00:26:41 +0800

    code

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -4,10 +4,8 @@ class Api::V1::BaseController < ApplicationController
   before_action :authenticate_user!, except: [ :index, :show, :login ]
 
   def authenticate_user!
-    p '@'*10
-    p request.headers['Content-Type'], request.headers['Authorization']
     token, options = ActionController::HttpAuthentication::Token.token_and_options(request)
-    p ActionController::HttpAuthentication::Token.token_and_options(request)
+    p token, options
     mobile_number = options.blank?? nil : options[:mobile_number]
     user = mobile_number && User.find_by(mobile_number: mobile_number)
 
