53aff3e714552b21be039fdda8ba12152abfbb31
zhangwei <719048757@qq.com>
2016-11-29 15:51:59 +0800

    code

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,17 +1,17 @@
 class Account < ApplicationRecord
-  has_secure_password
-
+  # has_secure_password
+  #
   # before_create :generate_authentication_token
-
-  def generate_authentication_token
-    loop do
-      self.authentication_token = SecureRandom.base64(64)
-      break if !Account.find_by(authentication_token: authentication_token)
-    end
-  end
-
-  def reset_auth_token!
-    generate_authentication_token
-    save
-  end
+  #
+  # def generate_authentication_token
+  #   loop do
+  #     self.authentication_token = SecureRandom.base64(64)
+  #     break if !Account.find_by(authentication_token: authentication_token)
+  #   end
+  # end
+  #
+  # def reset_auth_token!
+  #   generate_authentication_token
+  #   save
+  # end
 end
