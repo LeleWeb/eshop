5f7da100dc5b52faed612d62098d89a63aab584b
zhangwei <719048757@qq.com>
2016-12-01 18:38:33 +0800

    code

--- db/migrate/20161201103345_rename_object_to_owner_shopping_carts.rb
+++ db/migrate/20161201103345_rename_object_to_owner_shopping_carts.rb
@@ -1,4 +1,6 @@
 class RenameObjectToOwnerShoppingCarts < ActiveRecord::Migration[5.0]
   def change
+    rename_column(:shopping_carts, :object_id, :owner_id)
+    rename_column(:shopping_carts, :object_type, :owner_type)
   end
 end
