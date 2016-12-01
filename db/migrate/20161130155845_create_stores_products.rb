82bed39d177cb68d070e5dc84cf63e6bea545b03
zhangwei <719048757@qq.com>
2016-12-01 18:01:52 +0800

    code

--- db/migrate/20161130155845_create_stores_products.rb
+++ db/migrate/20161130155845_create_stores_products.rb
@@ -1,6 +1,6 @@
 class CreateStoresProducts < ActiveRecord::Migration[5.0]
   def change
-    create_table :stores_products do |t|
+    create_table :products_stores do |t|
       t.belongs_to :store
       t.belongs_to :product
       t.timestamps
