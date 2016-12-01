894be171de2e74226f183889bf17c5e7c9e66dd7
zhangwei <719048757@qq.com>
2016-12-01 22:41:34 +0800

    code

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,2 +1,42 @@
 class Api::V1::ProductsController < ApplicationController
+  before_action :set_account, only: [:show, :update, :destroy]
+
+  # GET /accounts
+  def index
+    render json: AccountsService.new.get_accounts
+  end
+
+  # GET /accounts/1
+  def show
+    authorize @account
+    render json: AccountsService.new.get_account(@account)
+  end
+
+  # POST /accounts
+  def create
+    render json: AccountsService.new.create_account(account_params)
+  end
+
+  # PATCH/PUT /accounts/1
+  def update
+    authorize @account
+    render json: AccountsService.new.update_account(@account, account_params)
+  end
+
+  # DELETE /accounts/1
+  def destroy
+    authorize @account
+    render json: AccountsService.new.destory_account(@account)
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_account
+    @account = Account.find(@account)
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def account_params
+    params.require(:account).permit(:uuid, :mobile_number, :email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :accounts
+      resources :accounts do
+        resources :stores do
+          resources :products
+        end
+      end
 
       # 登陆登出
       post 'login', to: 'session#login'
