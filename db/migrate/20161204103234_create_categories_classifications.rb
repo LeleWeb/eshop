8921578253717895244915bf55d77f0fb18e405b
zhangwei <719048757@qq.com>
2016-12-04 18:46:47 +0800

    code

--- db/migrate/20161204103234_create_categories_classifications.rb
+++ db/migrate/20161204103234_create_categories_classifications.rb
@@ -1,8 +1,8 @@
 class CreateCategoriesClassifications < ActiveRecord::Migration[5.0]
   def change
-    create_table :categories_classifications do |t|
-
-      t.timestamps
+    create_table :categories_classifications, id: false do |t|
+      t.belongs_to :Category
+      t.belongs_to :Classification
     end
   end
 end
