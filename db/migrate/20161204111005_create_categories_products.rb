d701c25569e7e3fac25062149aa02769bf2cb2e9
zhangwei <719048757@qq.com>
2016-12-04 19:25:54 +0800

    code

--- db/migrate/20161204111005_create_categories_products.rb
+++ db/migrate/20161204111005_create_categories_products.rb
@@ -1,8 +1,8 @@
 class CreateCategoriesProducts < ActiveRecord::Migration[5.0]
   def change
-    create_table :categories_products do |t|
-
-      t.timestamps
+    create_table :categories_products, id: false do |t|
+      t.belongs_to :category
+      t.belongs_to :product
     end
   end
 end
