c1f614140ed3b71a9542487159ee0125288d0357
zhangwei <719048757@qq.com>
2016-12-05 20:02:40 +0800

    code

--- db/migrate/20161205114040_create_collections.rb
+++ db/migrate/20161205114040_create_collections.rb
@@ -1,7 +1,8 @@
 class CreateCollections < ActiveRecord::Migration[5.0]
   def change
     create_table :collections do |t|
-      t.integer :product_id
+      t.string :object_type
+      t.integer :object_id
       t.string :owner_type
       t.integer :owner_id
       t.boolean :is_deleted
