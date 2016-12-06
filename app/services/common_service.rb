24612aa1ecbb439477887b0f5c8dd9f67e97f42a
zhangwei <719048757@qq.com>
2016-12-06 23:11:31 +0800

    code

--- app/services/common_service.rb
+++ app/services/common_service.rb
@@ -1,9 +1,10 @@
 class CommonService < BaseService
   def self.response_format(response_code, data=nil)
+    response = response_code.dump
     p response_code
     p data
     byebug
-    data.nil? ? response_code : response_code['data'] = data
-    response_code
+    data.nil? ? response : response[:data] = data
+    response
   end
 end
