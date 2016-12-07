684dc6ed54e7ca8a520b98a8ec3706b7dc3c1361
zhangwei <719048757@qq.com>
2016-12-07 16:32:54 +0800

    code

--- app/services/wechat_service.rb
+++ app/services/wechat_service.rb
@@ -1,6 +1,6 @@
 class WechatService < BaseService
   def get_wechat(wechat_params)
-    {:echostr => wechat_params["echostr"]}
+    wechat_params["echostr"]
     # CommonService.response_format(ResponseCode.COMMON.OK)
   end
 
