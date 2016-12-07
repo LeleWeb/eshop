b11f4ad3d0988a706d5ab904be133c1334deb8d5
zhangwei <719048757@qq.com>
2016-12-07 16:20:44 +0800

    code

--- app/controllers/api/v1/wechat_controller.rb
+++ app/controllers/api/v1/wechat_controller.rb
@@ -1,9 +1,18 @@
 class Api::V1::WechatController < Api::V1::BaseController
+  before_action :wechat_params, only: [:index]
+
   # GET /wechat
   def index
     p '@'*10
     p params
-    render json: WechatService.new.get_wechat
+    render json: WechatService.new.get_wechat(wechat_params)
+  end
+
+  private
+
+  # Only allow a trusted parameter "white list" through.
+  def wechat_params
+    params.permit(:signature, :echostr, :timestamp, :nonce)
   end
 
 end
--- app/services/wechat_service.rb
+++ app/services/wechat_service.rb
@@ -1,7 +1,7 @@
 class WechatService < BaseService
-  def get_wechat
-
-    CommonService.response_format(ResponseCode.COMMON.OK)
+  def get_wechat(wechat_params)
+    {:echostr => wechat_params["echostr"]}
+    # CommonService.response_format(ResponseCode.COMMON.OK)
   end
 
 end
