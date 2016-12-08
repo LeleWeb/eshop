6891b72d96f765ec7ea060c9ce5e0b164882ace8
zhangwei <719048757@qq.com>
2016-12-08 11:13:46 +0800

    code

--- app/controllers/api/v1/wechat_controller.rb
+++ app/controllers/api/v1/wechat_controller.rb
@@ -3,11 +3,7 @@ class Api::V1::WechatController < Api::V1::BaseController
 
   # GET /wechat
   def index
-    # p '@'*10
-    # p params
-    # p wechat_params["echostr"]
-    # puts wechat_params["echostr"].class
-    render plain: "108950072435990525"#wechat_params["echostr"] #WechatService.new.get_wechat(wechat_params)
+    render html: wechat_params["echostr"] #WechatService.new.get_wechat(wechat_params)
   end
 
   private
