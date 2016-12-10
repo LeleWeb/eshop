a3a4610637b45a75ccf6bd917c3a2e3829883cda
zhangwei <719048757@qq.com>
2016-12-10 17:28:11 +0800

    code

--- db/migrate/20161210080532_create_addresses.rb
+++ db/migrate/20161210080532_create_addresses.rb
@@ -1,7 +1,12 @@
 class CreateAddresses < ActiveRecord::Migration[5.0]
   def change
     create_table :addresses do |t|
-
+      t.integer :id
+      t.integer :customer_id
+      t.string  :mobile_number,:limit=>50
+      t.string  :detailed_address, :limit=>1024
+      t.boolean :is_default
+      t.string :remark, :limit => 255
       t.timestamps
     end
   end
