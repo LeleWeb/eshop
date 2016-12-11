b2b0007dab407026fecb5f8fab4d05dae3df14f4
zhangwei <719048757@qq.com>
2016-12-11 23:26:05 +0800

    code

--- app/services/http_service.rb
+++ app/services/http_service.rb
@@ -1,4 +1,6 @@
 require 'net/http'
+require 'net/https'
+require "uri"
 
 class HttpService < BaseService
   def self.get(url, query_params=nil)
@@ -13,19 +15,11 @@ class HttpService < BaseService
   end
 
   def self.post(url, params)
-    # uri = URI(url)
-    # res = Net::HTTP.post_form(uri, params)
-    # res.body
-
-    uri = URI(url)
-    req = Net::HTTP::Post.new(uri)
-    req.use_ssl = true if uri.scheme == "https"  # enable SSL/TLS
-    req.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    req.set_form_data(params)
-
-    res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-      http.request(req)
-    end
+    uri = URI.parse(url)
+    https = Net::HTTP.new(uri.host,uri.port)
+    https.use_ssl = true
+    req = Net::HTTP::Post.new(uri.path)
+    res = https.request(req)
 
     case res
       when Net::HTTPSuccess, Net::HTTPRedirection
@@ -33,6 +27,27 @@ class HttpService < BaseService
       else
         res.value
     end
+
+    # uri = URI(url)
+    # res = Net::HTTP.post_form(uri, params)
+    # res.body
+
+    # uri = URI(url)
+    # req = Net::HTTP::Post.new(uri)
+    # req.use_ssl = true if uri.scheme == "https"  # enable SSL/TLS
+    # req.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    # req.set_form_data(params)
+    #
+    # res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+    #   http.request(req)
+    # end
+    #
+    # case res
+    #   when Net::HTTPSuccess, Net::HTTPRedirection
+    #     res.body
+    #   else
+    #     res.value
+    # end
   end
 
 end
