56d58e1b52d7acd043e0769f9fdcbb87c57cfcf5
zhangwei <719048757@qq.com>
2016-12-11 23:34:03 +0800

    code

--- app/services/qrcode_service.rb
+++ app/services/qrcode_service.rb
@@ -1,6 +1,6 @@
 class QrcodeService < BaseService
   def create_qrcode
-    res = HttpService.post(Settings.WECHAT.CREATE_QRCODE_URL + "?access_token=72wGq0nbaDJBR_E4lxeq-TsirFIGSkF6XumLH6HFYF4DaOueuH8_Ii0LVdMQZXYy7t_CgrmkCyPqCEw1UEp5NTO1q2LZvq_D-7BPEv2L0AKrQeH4WEQfirqMcgBNw4HiYZCbAHADEH",
+    res = HttpService.post(Settings.WECHAT.CREATE_QRCODE_URL + "?access_token=U2EyPdV705JIOyeZpeMOKKpidciG-24jzGZiDBGY_SDnVUObZYgUCyGMccL8dX8HRhCDdAqxsRafxo_qS0KMqQ8Y5b1YyTYbpkB-H_aMSGxJDDxMEDbL19SqL2YLR_oCZQHfAEAOFW",
                            Settings.WECHAT.FOREVER_QRCODE_POST_PARAMS.QRCODE_POST_PARAMS_STR)
     puts res
     res
