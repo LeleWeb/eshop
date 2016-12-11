93f405b833db452f07cd78322030b3af3042beee
zhangwei <719048757@qq.com>
2016-12-11 23:10:42 +0800

    code

--- app/services/http_service.rb
+++ app/services/http_service.rb
@@ -13,24 +13,26 @@ class HttpService < BaseService
   end
 
   def self.post(url, params)
-    uri = URI(url)
-    res = Net::HTTP.post_form(uri, params)
-    res.body
-
     # uri = URI(url)
-    # req = Net::HTTP::Post.new(uri)
-    # req.set_form_data(params)
-    #
-    # res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-    #   http.request(req)
-    # end
-    #
-    # case res
-    #   when Net::HTTPSuccess, Net::HTTPRedirection
-    #     res.body
-    #   else
-    #     res.value
-    # end
+    # res = Net::HTTP.post_form(uri, params)
+    # res.body
+
+    uri = URI(url)
+    req = Net::HTTP::Post.new(uri)
+    req.use_ssl = true if uri.scheme == "https"  # enable SSL/TLS
+    req.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    req.set_form_data(params)
+
+    res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+      http.request(req)
+    end
+
+    case res
+      when Net::HTTPSuccess, Net::HTTPRedirection
+        res.body
+      else
+        res.value
+    end
   end
 
 end
