705582e6a13c0bca01b673b5e65bad23de525511
zhangwei <719048757@qq.com>
2016-12-13 17:10:27 +0800

    code

--- db/migrate/20161213090726_create_orders_products.rb
+++ db/migrate/20161213090726_create_orders_products.rb
@@ -1,6 +1,6 @@
 class CreateOrdersProducts < ActiveRecord::Migration[5.0]
   def change
-    create_table :orders_products do |t|
+    create_table :orders_products, id: false do |t|
       t.belongs_to :order
       t.belongs_to :product
     end
