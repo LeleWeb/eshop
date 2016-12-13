678098b9666d1cebc8764398f5119368aa2e8bd8
zhangwei <719048757@qq.com>
2016-12-13 15:14:18 +0800

    code

--- db/migrate/20161213071022_modify_uuid_length.rb
+++ db/migrate/20161213071022_modify_uuid_length.rb
@@ -1,4 +1,7 @@
 class ModifyUuidLength < ActiveRecord::Migration[5.0]
   def change
+    change_column(:accounts, :uuid, :string, limit: 255)
+    change_column(:orders, :uuid, :string, limit: 255)
+    change_column(:products, :uuid, :string, limit: 255)
   end
 end
