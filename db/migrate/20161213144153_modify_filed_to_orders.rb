0372cdad9178e00d2e307967faa4ca36bd9c6857
zhangwei <719048757@qq.com>
2016-12-13 23:02:22 +0800

    code

--- db/migrate/20161213144153_modify_filed_to_orders.rb
+++ db/migrate/20161213144153_modify_filed_to_orders.rb
@@ -1,4 +1,13 @@
 class ModifyFiledToOrders < ActiveRecord::Migration[5.0]
   def change
+    remove_columns(:orders, :product_id, :unit_price, :amount, :seller_id, :seller_type)
+    rename_column(:orders, :uuid, :order_number)
+    change_column(:orders, :order_number, :string, limit: 256)
+    add_column(:orders, :pay_away, :integer)
+    add_column(:orders, :time_start, :datetime)
+    add_column(:orders, :time_expire, :datetime)
+    add_column(:orders, :consignee_name, :string, limit: 32)
+    add_column(:orders, :consignee_phone, :string, limit: 32)
+    add_column(:orders, :consignee_address, :string, limit: 256)
   end
 end
