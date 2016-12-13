540bdbe265fb1b5c0033a95b007d14390fb3591d
zhangwei <719048757@qq.com>
2016-12-13 23:07:10 +0800

    code

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,5 @@ class Order < ApplicationRecord
   has_and_belongs_to_many :products
   # belongs_to :seller, polymorphic: true
   belongs_to :buyer, polymorphic: true
+  has_many :order_details
 end
