a70bd0ec760143b343dd94959599ca0f70ba57a0
zhangwei <719048757@qq.com>
2016-12-14 23:41:02 +0800

    code

--- app/controllers/api/v1/wx_page_authorization_controller.rb
+++ app/controllers/api/v1/wx_page_authorization_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::WxPageAuthorizationController < ApplicationController
+  before_action :set_query_params, only: [:index]
+
+  # GET api/v1/wx_page_authorization
+  def index
+    p '@'*10
+    p params
+    #render html: WechatService.new.get_wechat(wechat_params)
+  end
+
+  private
+
+  # Only allow a trusted parameter "white list" through.
+  def set_query_params
+    @query_params = params.permit(:code, :state)
+  end
 end
