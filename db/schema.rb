385b4fe508afd713a1c608e940f03175b57c7d13
zhangwei <719048757@qq.com>
2016-12-15 18:28:21 +0800

    code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161213144225) do
+ActiveRecord::Schema.define(version: 20161214030923) do
 
   create_table "accounts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "uuid"
@@ -149,6 +149,7 @@ ActiveRecord::Schema.define(version: 20161213144225) do
     t.string   "consignee_name",    limit: 32
     t.string   "consignee_phone",   limit: 32
     t.string   "consignee_address", limit: 256
+    t.float    "pay_price",         limit: 24
   end
 
   create_table "orders_products", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
