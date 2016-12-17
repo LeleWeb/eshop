85a0268bc6bdb9ad97129450c6dac63fbb1b45b5
zhangwei <719048757@qq.com>
2016-12-17 16:08:53 +0800

    code

--- db/migrate/20161217075958_modify_total_price_int_orders.rb
+++ db/migrate/20161217075958_modify_total_price_int_orders.rb
@@ -1,5 +1,5 @@
 class ModifyTotalPriceIntOrders < ActiveRecord::Migration[5.0]
   def change
-    change_column(:orders, :total_price, :float, :integer)
+    change_column(:orders, :total_price, :integer)
   end
 end
