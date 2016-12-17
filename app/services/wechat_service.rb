c5405fba4ba8d84ea76ce54ed243d491fc08e952
zhangwei <719048757@qq.com>
2016-12-17 23:49:13 +0800

    code

--- app/services/wechat_service.rb
+++ app/services/wechat_service.rb
@@ -215,14 +215,14 @@ class WechatService < BaseService
   def self.generate_jsapi_params(prepay_res)
     temp_time = self.generate_timeStamp/1000
     temp_str = self.generate_nonce_str
-    signature_params = Settings.WECHAT.JSAPI_PAY_SIGNATURE_PARAMS.as_json
-    signature_params["jsapi_ticket"] = self.get_jsapi_ticket
-    signature_params["url"] = "http://www.yiyunma.com/"
-    signature_params["timestamp"] = temp_time
-    signature_params["noncestr"] = temp_str
-    signature = self.generate_jsapi_sign(signature_params)
-    puts "#"*10
-    p signature_params
+    # signature_params = Settings.WECHAT.JSAPI_PAY_SIGNATURE_PARAMS.as_json
+    # signature_params["jsapi_ticket"] = self.get_jsapi_ticket
+    # signature_params["url"] = "http://www.yiyunma.com/"
+    # signature_params["timestamp"] = temp_time
+    # signature_params["noncestr"] = temp_str
+    # signature = self.generate_jsapi_sign(signature_params)
+    # puts "#"*10
+    # p signature_params
 
     data = {}
     data["appid"] = prepay_res["appid"]
@@ -230,8 +230,8 @@ class WechatService < BaseService
     data["noncestr"] = temp_str
     data["package"] = "prepay_id=#{prepay_res["prepay_id"]}"
     data["signtype"] = "SHA1"
-    data["paysign"] = signature
-    data["jsapi_ticket"] = signature_params["jsapi_ticket"]
+    data["paysign"] = self.generate_sign(data, "Digest::SHA1")#signature
+    #data["jsapi_ticket"] = signature_params["jsapi_ticket"]
     data
   end
 
