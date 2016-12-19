8bb77bda73e056f18b89df55887e41ccb4e59357
zhangwei <719048757@qq.com>
2016-12-19 22:58:02 +0800

    code

--- app/controllers/api/v1/wechat_controller.rb
+++ app/controllers/api/v1/wechat_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::WechatController < ApplicationController
 
   # GET /wechat
   def index
-    render html: WechatService.new.get_wechat(wechat_params)
+    WechatService.new.get_wechat(wechat_params)
+    redirect_to "http://www.yiyunma.com/#/home"
   end
 
   # POST /accounts
