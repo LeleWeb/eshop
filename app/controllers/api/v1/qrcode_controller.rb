c23242dad1ed4bcf71fae1a0f58672053903319b
zhangwei <719048757@qq.com>
2016-12-19 22:35:21 +0800

    code

--- app/controllers/api/v1/qrcode_controller.rb
+++ app/controllers/api/v1/qrcode_controller.rb
@@ -1,10 +1,8 @@
 class Api::V1::QrcodeController < Api::V1::BaseController
-  #before_action :qrcode_params, only: [:create]
+  before_action :qrcode_params, only: [:create]
 
   # POST /qrcode
   def create
-    puts 'b'*10
-    p params
     render plain: QrcodeService.new.create_qrcode, content_type: "image/jpg"
   end
 
@@ -12,8 +10,6 @@ class Api::V1::QrcodeController < Api::V1::BaseController
 
   # Only allow a trusted parameter "white list" through.
   def qrcode_params
-    puts 'zw'*10
-    p params
     params#.permit()
   end
 end
--- app/services/qrcode_service.rb
+++ app/services/qrcode_service.rb
@@ -7,17 +7,12 @@ class QrcodeService < BaseService
     res = JSON.parse(HttpService.post(Settings.WECHAT.CREATE_QRCODE_URL + "?access_token=#{WechatService.read_access_token}",
                            Settings.WECHAT.FOREVER_QRCODE_POST_PARAMS.QRCODE_POST_PARAMS_STR.to_json,
                            req_headers))
-    puts "a"*10
-    puts res
 
     if res.key?("ticket") && !res["ticket"].blank?
       # 通过ticket换取二维码
       # HTTP GET请求（请使用https协议）https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=TICKET提醒：TICKET记得进行UrlEncode
       query_parmas = Settings.WECHAT.SHOW_QRCODE.QUERY_PARAMS.as_json
       query_parmas["ticket"] = res["ticket"]
-      res = HttpService.get(Settings.WECHAT.SHOW_QRCODE.URL, query_parmas)
-      puts "1"*10
-      p res
       res
     end
   end
