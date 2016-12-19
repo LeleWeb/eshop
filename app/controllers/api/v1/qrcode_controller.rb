3aaa6b2e6e6d5053169afed973333c0909fc6859
zhangwei <719048757@qq.com>
2016-12-19 21:31:50 +0800

    code

--- app/controllers/api/v1/qrcode_controller.rb
+++ app/controllers/api/v1/qrcode_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::QrcodeController < Api::V1::BaseController
-  before_action :qrcode_params, only: [:create]
+  #before_action :qrcode_params, only: [:create]
 
   # POST /qrcode
   def create
+    puts 'b'*10
+    p params
     render plain: QrcodeService.new.create_qrcode#, content_type: "image/jpg"
   end
 
