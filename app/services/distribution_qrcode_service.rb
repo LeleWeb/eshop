7922c2e88970dbfc10d3b619470690c494ac5a6c
zhangwei <719048757@qq.com>
2016-12-21 02:22:59 +0800

    code

--- app/services/distribution_qrcode_service.rb
+++ app/services/distribution_qrcode_service.rb
@@ -4,7 +4,7 @@ class DistributionQrcodeService < BaseService
     query_params = Settings.DISTRIBUTION_QRCODE.CLI_CREATE_QRCODE.QUERY_PARAMS.as_json
     query_params["text"] = "http://www.yiyunma.com/#/detail?product_id=1"
     res_string = HttpService.get(Settings.WECHAT.PAGE_ACCESS_TOKEN.URL, query_params)
-    CommonService.response_format(ResponseCode.COMMON.OK, self.parse_qrcode_img(res_string))
+    CommonService.response_format(ResponseCode.COMMON.OK, DistributionQrcodeService.parse_qrcode_img(res_string))
   end
 
   # 草料生成二维码的接口返回的是html格式数据，需要解析出图片的地址按照json格式返回给前端。
