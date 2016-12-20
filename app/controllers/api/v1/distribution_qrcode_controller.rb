3d8d711c963154ee552eba5cd5a0afcab580a933
zhangwei <719048757@qq.com>
2016-12-21 02:40:20 +0800

    code

--- app/controllers/api/v1/distribution_qrcode_controller.rb
+++ app/controllers/api/v1/distribution_qrcode_controller.rb
@@ -1,16 +1,21 @@
 class Api::V1::DistributionQrcodeController < Api::V1::BaseController
   #before_action :distribution_qrcode_params, only: [:create]
+  before_action :set_product, only: [:create]
 
   # 基于草料二维码实现产品分销二维码功能 http://cli.im/
   def create
-    render json: DistributionQrcodeService.new.create_qrcode
+    render json: DistributionQrcodeService.new.create_qrcode(@product)
     # render plain: DistributionQrcodeService.new.create_qrcode, content_type: "image/jpg"
   end
 
   private
 
-  # Only allow a trusted parameter "white list" through.
-  def distribution_qrcode_params
-    params#.permit()
+  def set_product
+    @product = Product.find(params[:product_id])
   end
+
+  # Only allow a trusted parameter "white list" through.
+  # def distribution_qrcode_params
+  #   params#.permit()
+  # end
 end
--- app/services/distribution_qrcode_service.rb
+++ app/services/distribution_qrcode_service.rb
@@ -1,16 +1,14 @@
 class DistributionQrcodeService < BaseService
-  def create_qrcode
+  def create_qrcode(product)
     # 调用草料二维码来生成，后期可以将源码直接集成到本系统。
     query_params = Settings.DISTRIBUTION_QRCODE.CLI_CREATE_QRCODE.QUERY_PARAMS.as_json
-    query_params["text"] = "http://www.yiyunma.com/#/detail?product_id=1"
+    query_params["text"] = "http://www.yiyunma.com/#/detail?product_id=#{product.id}"
     res_string = HttpService.get(Settings.DISTRIBUTION_QRCODE.CLI_CREATE_QRCODE.URL, query_params)
     CommonService.response_format(ResponseCode.COMMON.OK, DistributionQrcodeService.parse_qrcode_img(res_string))
   end
 
   # 草料生成二维码的接口返回的是html格式数据，需要解析出图片的地址按照json格式返回给前端。
   def self.parse_qrcode_img(str)
-    puts 'a'*10
-    p str, str =~ /<img src=\"([^\"]*)\"/
     if str =~ /<img src=\"([^\"]*)\"/
       $1
     else
