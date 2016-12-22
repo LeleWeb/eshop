bf8476e43ba55322eef770366815b7617475df2c
zhangwei <719048757@qq.com>
2016-12-22 16:07:08 +0800

    code

--- app/controllers/api/v1/wxpay_controller.rb
+++ app/controllers/api/v1/wxpay_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::WxpayController < Api::V1::BaseController
   #before_action :wxpay_params, only: [:create]
 
-  # 
+  #
+  def index
+    puts 'm'*10
+    p params
+    # render plain: QrcodeService.new.create_qrcode, content_type: "image/jpg"
+  end
+
+  #
   def create
     puts 'n'*10
     p params
