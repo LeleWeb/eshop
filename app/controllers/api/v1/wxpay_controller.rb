d117a1f3ed67997d396a686fa81f5c9936f78fa3
zhangwei <719048757@qq.com>
2016-12-22 19:37:46 +0800

    code

--- app/controllers/api/v1/wxpay_controller.rb
+++ app/controllers/api/v1/wxpay_controller.rb
@@ -3,7 +3,9 @@ class Api::V1::WxpayController < Api::V1::BaseController
 
   # 微信支付结果通用通知接受接口
   def create
-    render xml: WxpayService.new.create_notify(wxpay_params)
+    puts 'q'*10
+    p temp = WxpayService.new.create_notify(wxpay_params)
+    render xml: temp
   end
 
   private
