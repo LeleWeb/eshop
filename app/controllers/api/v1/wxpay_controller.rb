1f84cb1ad5917b77980beea7509345800c7de863
zhangwei <719048757@qq.com>
2016-12-22 19:51:24 +0800

    code

--- app/controllers/api/v1/wxpay_controller.rb
+++ app/controllers/api/v1/wxpay_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::WxpayController < Api::V1::BaseController
   before_action :wxpay_params, only: [:create]
+  skip_before_action :authenticate_user!
 
   # 微信支付结果通用通知接受接口
   def create
