71215662afa24d89d2d588bbc6f372b20162ad21
zhangwei <719048757@qq.com>
2016-12-23 10:00:21 +0800

    code

--- app/services/common_service.rb
+++ app/services/common_service.rb
@@ -1,7 +1,17 @@
+require 'parseexcel'
+
 class CommonService < BaseService
   def self.response_format(response_code, data=nil)
     response = response_code.as_json
     data.nil? ? response : response[:data] = data
     response
+  end
+
+  def self.import_products(file_path=Rails.root + "products.xlsx")
+    workbook = Spreadsheet::ParseExcel.parse(file_path)
+    products_sheet = workbook.worksheet(0)
+    worksheet.each(skip) do |row|
+      puts row
+    end
   end
 end
