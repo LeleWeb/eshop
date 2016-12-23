b060dd5266fccd490269181655c810ae1417861a
zhangwei <719048757@qq.com>
2016-12-23 13:46:37 +0800

    code

--- app/services/common_service.rb
+++ app/services/common_service.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class CommonService < BaseService
   def self.response_format(response_code, data=nil)
     response = response_code.as_json
@@ -6,9 +8,69 @@ class CommonService < BaseService
   end
 
   def self.import_products(file_path="/var/www/eshop/test.csv")
+    picture_category = {"1" => 1,
+                        "2" => 1,
+                        "3" => 1,
+                        "4" => 2,
+                        "5" => 4,
+                        "6" => 3,
+                        "7" => 3,
+                        "8" => 3,
+                        "9" => 3,
+                        "10" => 3}
+
     CSV.foreach(file_path) do |row|
-      # use row here...
-      puts row
+      begin
+        Client.transaction do
+          # 读取数据rows
+          if !row.nil? && row[0] =~ /^\d+&/
+            # 数据库创建商品
+            store = Store.find_by(name: "环球捕手")
+            product_params = {
+                "product"=> {
+                    "uuid"=> SecureRandom.hex,
+                    "name"=> row[1],
+                    "description"=> row[2],
+                    "detail"=> row[3],
+                    "stock"=> row[4],
+                    "price"=> row[5],
+                    "real_price"=> row[6],
+                    "category_id"=> row[7]
+                }
+            }
+            product = ProductsService.new.create_product(store, product_params)
+
+            # 数据库创建商品图片
+            product_picture_dir = row[8].gsub(/\\/, '/')
+            # 遍历目录下的所有图片文件
+            if File.directory?("#{Rails.root}/public/images/huanqiubushou/products/#{product_picture_dir}")
+              Dir.foreach(filepath) do |filename|
+                if filename =~ /(.*)(\d+)\.(jpg|png)$/
+                  # 解析图片分类
+                  picture_name = $1
+                  category_number = $2
+
+                  picture_params = {
+                      "picture"=> {
+                          "name"=> picture_name,
+                          "url"=> filename.gsub("#{Rails.root}/public", ''),
+                          "category"=> picture_category[category_number],
+                      },
+                      "owner_id"=> product.id,
+                      "owner_type"=> "Product"
+                  }
+                  PicturesService.new.create_picture(product, picture_params)
+
+                end
+              end
+            else
+              raise "error: Product: #{row[1]} picture url is not directory!"
+            end
+          end
+        end
+      rescue
+        next
+      end
     end
   end
 end
