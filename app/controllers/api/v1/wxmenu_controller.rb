b3f0ba3d4baadd418c7d382fdafdeeab6dfe302d
zhangwei <719048757@qq.com>
2016-12-23 17:56:41 +0800

    code

--- app/controllers/api/v1/wxmenu_controller.rb
+++ app/controllers/api/v1/wxmenu_controller.rb
@@ -1,9 +1,11 @@
 class Api::V1::WxmenuController < Api::V1::BaseController
-  before_action :wxmenu_params, only: [:create]
+  #before_action :wxmenu_params, only: [:create]
   skip_before_action :authenticate_user!
 
   # 微信支付结果通用通知接受接口
   def create
+    p 'b'*10
+    p params
     render json: WxmenuService.new.create_wxmenu(wxmenu_params)
   end
 
--- app/services/wxmenu_service.rb
+++ app/services/wxmenu_service.rb
@@ -1,7 +1,7 @@
 class WxmenuService < BaseService
   def create_wxmenu(wxmenu_params)
     puts 'a'*10
-    p wxmenu_params
+    p wxmenu_params["wxmenu"]
     # 调用微信创建菜单接口
     req_headers = [
         {:key => Settings.REQUEST_HEADERS.CONTENT_TYPE_KEY, :value => Settings.REQUEST_HEADERS.CONTENT_TYPE_VALUE.JSON}
