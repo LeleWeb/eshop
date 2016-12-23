5e85ba3eee0264020d14a6b17a1f1ff9c81c7ad1
zhangwei <719048757@qq.com>
2016-12-23 17:32:15 +0800

    code

--- app/controllers/api/v1/wxmenu_controller.rb
+++ app/controllers/api/v1/wxmenu_controller.rb
@@ -1,2 +1,16 @@
-class Api::V1::WxmenuController < ApplicationController
+class Api::V1::WxmenuController < Api::V1::BaseController
+  before_action :wxmenu_params, only: [:create]
+  skip_before_action :authenticate_user!
+
+  # 微信支付结果通用通知接受接口
+  def create
+    render json: WxmenuService.new.create_wxmenu(wxmenu_params)
+  end
+
+  private
+
+  # Only allow a trusted parameter "white list" through.
+  def wxmenu_params
+    params
+  end
 end
---
+++ app/services/wxmenu_service.rb
@@ -0,0 +1,12 @@
+class WxmenuService < BaseService
+  def create_wxmenu(wxmenu_params)
+    # 调用微信创建菜单接口
+    req_headers = [
+        {:key => Settings.REQUEST_HEADERS.CONTENT_TYPE_KEY, :value => Settings.REQUEST_HEADERS.CONTENT_TYPE_VALUE.JSON}
+    ]
+    JSON.parse(HttpService.post(Settings.WECHAT.CREATE_WXMENU_URL + "?access_token=#{WechatService.read_access_token}",
+                                      wxmenu_params.to_json,
+                                      req_headers))
+  end
+
+end
