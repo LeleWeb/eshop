21e5de4e8dd5f4bd3f09c7db1c8b24dfb146a48c
zhangwei <719048757@qq.com>
2016-12-23 20:27:51 +0800

    code

--- app/services/common_service.rb
+++ app/services/common_service.rb
@@ -20,49 +20,50 @@ class CommonService < BaseService
                         "9" => 3,
                         "10" => 3}
 
+    store = Store.find_by(name: "环球捕手")
     CSV.foreach("/var/www/eshop/products.csv") do |row|
       # begin
-        #Product.transaction do
+        Product.transaction do
           # 读取数据rows
           if !row.nil? && row[0] =~ /^\d+$/
-            # 数据库创建商品
-            store = Store.find_by(name: "环球捕手")
-            product_params = {
-              "uuid"=> SecureRandom.hex,
-              "name"=> row[1],
-              "description"=> row[2],
-              "detail"=> row[3],
-              "stock"=> row[4],
-              "price"=> row[5],
-              "real_price"=> row[6],
-              "category_id"=> row[7]
-            }
-            product = store.products.create(product_params)
-            # 数据库创建商品图片
-            product_picture_dir = row[8].gsub(/\\/, '/')
-
+            p row
+            # # 数据库创建商品
+            # product_params = {
+            #   "uuid"=> SecureRandom.hex,
+            #   "name"=> row[1],
+            #   "description"=> row[2],
+            #   "detail"=> row[3],
+            #   "stock"=> row[4],
+            #   "price"=> row[5],
+            #   "real_price"=> row[6],
+            #   "category_id"=> row[7]
+            # }
+            # product = store.products.create(product_params)
+            # # 数据库创建商品图片
+            # product_picture_dir = row[8].gsub(/\\/, '/')
+            #
             # 遍历目录下的所有图片文件
             pictures_dir = "#{Rails.root}/public/images/huanqiubushou/products/#{product_picture_dir}"
             if File.directory?(pictures_dir)
               Dir.foreach(pictures_dir) do |filename|
                 if filename =~ /(.*)(\d+)\.(jpg|png)$/
-                  # 解析图片分类
-                  picture_name = $1
-                  category_number = $2
-                  picture_params = {
-                    "name"=> picture_name,
-                    "url"=> filename.gsub("#{Rails.root}/public", ''),
-                    "category"=> picture_category[category_number]
-                  }
-                  PicturesService.new.create_picture(product, picture_params)
-
+                  p filename
+                  # # 解析图片分类
+                  # picture_name = $1
+                  # category_number = $2
+                  # picture_params = {
+                  #   "name"=> picture_name,
+                  #   "url"=> filename.gsub("#{Rails.root}/public", ''),
+                  #   "category"=> picture_category[category_number]
+                  # }
+                  # PicturesService.new.create_picture(product, picture_params)
                 end
               end
             else
               raise "error: Product: #{row[1]} picture url is not directory!"
             end
           end
-        #end
+        end
       # rescue
       #   next
       # end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # 账户种子数据
-accounts = Account.create([
-                        {
-                            mobile_number: '18161803190',
-                            email: '719048757@qq.com'
-                        },
-                        {
-                            mobile_number: '18717373045',
-                            email: '514079588@qq.com'
-                        },
-                        {
-                            mobile_number: '18629365871',
-                            email: '344410812@qq.com'
-                        }
-                    ])
+# account = Account.create({
+#                             mobile_number: '13758949777',
+#                             email: '3407586904@qq.com'
+#                         })
+account = Account.first
 
 # 账户角色种子数据
 roles = Role.create([
@@ -39,76 +30,11 @@ roles = Role.create([
                     ])
 
 # 账户角色设置
-accounts.each do |account|
-  AccountsRole.create(account_id: account.id, role_id: Role.find_by(name: 'customer').id)
-  AccountsRole.create(account_id: account.id, role_id: Role.find_by(name: 'store').id)
-  AccountsRole.create(account_id: account.id, role_id: Role.find_by(name: 'administrator').id)
-end
-
-account = accounts.first
-
-# 创建消费者
-customer = account.create_customer(mobile_number: "18161803190",
-                        nick_name: "bluesky",
-                        real_name: "张伟",
-                        gender: "男",
-                        age: 30,
-                        address: "西安市高新区",
-                        is_wechat_focus: true,
-                        level: 0,
-                        integral: 0)
+AccountsRole.create(account_id: account.id, role_id: Role.find_by(name: 'store').id)
 
 # 创建商家
 store = account.create_store(name: "环球捕手",
-                     description: "微商城",
-                     # type: 1,
-                     address: "西安高新",
-                     mobile_number: "18112345678")
-
-# 创建产品
-product = store.products.create([{name: "小米4",
-                                   description: "屌丝神器",
-                                   detail: "商品详情",
-                                   stock: 100,
-                                   price: 1999.0,
-                                   real_price: 999.12,
-                                   status: 0}
-                                 ])
-
-# 产品详情项
-detail_items = DetailItem.create([
-                                     { name: "品牌" },
-                                     { name: "尺寸" }
-                                ])
-
-sproduct = product[0]
-# 创建产品详情
-sproduct.product_details.create([
-                                   { detail_item_id: detail_items[0].id, content: "小米"},
-                                   { detail_item_id: detail_items[1].id, content: "5.5寸"}
-                               ])
-
-# 图片资源
-
-# 订单
-store.orders.create(product_id: sproduct.id,
-                    unit_price: sproduct.price,
-                    amount: 10,
-                    status: 0,
-                    total_price: 19990.0,
-                    seller_id: store.id,
-                    seller_type: "Store",
-                    estimate: 0)
-
-customer.orders.create(product_id: sproduct.id,
-                      unit_price: sproduct.price,
-                      amount: 10,
-                      status: 0,
-                      total_price: 19990.0,
-                      seller_id: store.id,
-                      seller_type: "Store",
-                      estimate: 0)
-
-# 购物车
-customer.shopping_carts.create(product_id: sproduct.id,
-                               amount: 10)
+                             description: "微商城",
+                             # type: 1,
+                             address: "杭州",
+                             mobile_number: "13758949777")
