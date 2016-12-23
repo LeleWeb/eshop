69bd327ad90cec3cc725bef4dc0ce5106de5074e
zhangwei <719048757@qq.com>
2016-12-23 22:18:44 +0800

    code

--- app/services/wxmenu_service.rb
+++ app/services/wxmenu_service.rb
@@ -1,7 +1,7 @@
 class WxmenuService < BaseService
   def create_wxmenu(wxmenu_params)
     puts 'a'*10
-    p wxmenu_params.to_s.gsub(/=>/, ':')
+    p wxmenu_params.to_h.to_s.gsub(/=>/, ':')
     # 调用微信创建菜单接口
     req_headers = [
         {:key => Settings.REQUEST_HEADERS.CONTENT_TYPE_KEY, :value => Settings.REQUEST_HEADERS.CONTENT_TYPE_VALUE.JSON}
