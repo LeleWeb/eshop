a808470582705dc33e86a00e2773387128eb9657
zhangwei <719048757@qq.com>
2016-12-24 21:02:48 +0800

    code

--- db/migrate/20161224125209_create_order_log.rb
+++ db/migrate/20161224125209_create_order_log.rb
@@ -1,6 +1,13 @@
 class CreateOrderLog < ActiveRecord::Migration[5.0]
   def change
     create_table :order_logs do |t|
+      t.integer :order_id
+      t.integer :operator_type
+      t.integer :operator_id
+      t.integer :action_number
+      t.datetime :operate_time
+
+      t.timestamps
     end
   end
 end
--- db/migrate/20161224125302_add_fileds_orders.rb
+++ db/migrate/20161224125302_add_fileds_orders.rb
@@ -1,4 +1,6 @@
 class AddFiledsOrders < ActiveRecord::Migration[5.0]
   def change
+    add_column :orders, :shipping_type, :integer
+    add_column :orders, :shipping_number, :integer
   end
 end
