baf9696bf006e5a519a825e68871ba9e0d353e78
zhangwei <719048757@qq.com>
2016-12-24 21:11:22 +0800

    code

--- db/migrate/20161224125302_add_fileds_orders.rb
+++ db/migrate/20161224125302_add_fileds_orders.rb
@@ -1,6 +1,6 @@
 class AddFiledsOrders < ActiveRecord::Migration[5.0]
   def change
     add_column :orders, :shipping_type, :integer
-    add_column :orders, :shipping_number, :integer
+    add_column :orders, :shipping_number, :string, :limit => 50
   end
 end
