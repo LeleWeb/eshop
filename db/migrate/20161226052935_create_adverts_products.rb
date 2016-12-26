93f4fe06109bfa6818d31d7e6b0975d6380d4bea
zhangwei <719048757@qq.com>
2016-12-26 13:30:50 +0800

    code

--- db/migrate/20161226052935_create_adverts_products.rb
+++ db/migrate/20161226052935_create_adverts_products.rb
@@ -1,6 +1,8 @@
 class CreateAdvertsProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :adverts_products do |t|
+      t.belongs_to :adverts
+      t.belongs_to :products
     end
   end
 end
