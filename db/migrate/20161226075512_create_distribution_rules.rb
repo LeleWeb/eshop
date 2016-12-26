165166a3abd55183f54f792536226ee809d9e0ea
zhangwei <719048757@qq.com>
2016-12-26 16:21:38 +0800

    code

--- db/migrate/20161226075512_create_distribution_rules.rb
+++ db/migrate/20161226075512_create_distribution_rules.rb
@@ -1,6 +1,11 @@
 class CreateDistributionRules < ActiveRecord::Migration[5.0]
   def change
     create_table :distribution_rules do |t|
+      t.integer :store_id
+      t.string :name, :limit => 256
+      t.integer :category
+      t.string :value, :limit => 256
+      t.string :remark, :limit => 256
 
       t.timestamps
     end
