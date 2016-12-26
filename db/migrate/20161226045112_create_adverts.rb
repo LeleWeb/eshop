ec4315a68d44073715b25c381214643360f98f9a
zhangwei <719048757@qq.com>
2016-12-26 13:09:33 +0800

    code

--- db/migrate/20161226045112_create_adverts.rb
+++ db/migrate/20161226045112_create_adverts.rb
@@ -1,6 +1,15 @@
 class CreateAdverts < ActiveRecord::Migration[5.0]
   def change
     create_table :adverts do |t|
+      t.integer :store_id
+      t.string :img_url, :limit => 256
+      t.string :title, :limit => 256
+      t.string :description, :limit => 256
+      t.string :link_url, :limit => 256
+      t.integer :status
+      t.integer :category
+      t.string :remark, :limit => 256
+      t.string
 
       t.timestamps
     end
