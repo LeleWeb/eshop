e78f2038a078e75ccc8292aac030185321cf0880
zhangwei <719048757@qq.com>
2016-12-29 02:03:36 +0800

    code

--- app/services/distributions_service.rb
+++ app/services/distributions_service.rb
@@ -129,7 +129,7 @@ class DistributionsService < BaseService
     # 3.去distribution_levels表找到第二布计算的总额所在区间等级记录，将总额*佣金系数得到个人佣金余额；
     distribution_level = DistributionLevel.where("minimum >= ? and maximum < ? ", consume_sum, consume_sum)
     if !distribution_level.nil?
-      commission = distribution_level*commission_ratio
+      commission = consume_sum*distribution_level.commission_ratio
     end
     commission
   end
