66f5c5c99c0acd2f95607fd7a7d44bef3417a9af
zhangwei <719048757@qq.com>
2016-12-29 01:26:49 +0800

    code

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,30 +11,34 @@
 #                             mobile_number: '13758949777',
 #                             email: '3407586904@qq.com'
 #                         })
-account = Account.first
+# account = Account.first
+#
+# # 账户角色种子数据
+# roles = Role.create([
+#                         {
+#                             name: 'customer',
+#                             remark: '消费者角色'
+#                         },
+#                         {
+#                             name: 'store',
+#                             remark: '商家角色'
+#                         },
+#                         {
+#                             name: 'administrator',
+#                             remark: '系统管理员角色'
+#                         }
+#                     ])
+#
+# # 账户角色设置
+# AccountsRole.create(account_id: account.id, role_id: Role.find_by(name: 'store').id)
+#
+# # 创建商家
+# store = account.create_store(name: "环球捕手",
+#                              description: "微商城",
+#                              # type: 1,
+#                              address: "杭州",
+#                              mobile_number: "13758949777")
 
-# 账户角色种子数据
-roles = Role.create([
-                        {
-                            name: 'customer',
-                            remark: '消费者角色'
-                        },
-                        {
-                            name: 'store',
-                            remark: '商家角色'
-                        },
-                        {
-                            name: 'administrator',
-                            remark: '系统管理员角色'
-                        }
-                    ])
-
-# 账户角色设置
-AccountsRole.create(account_id: account.id, role_id: Role.find_by(name: 'store').id)
-
-# 创建商家
-store = account.create_store(name: "环球捕手",
-                             description: "微商城",
-                             # type: 1,
-                             address: "杭州",
-                             mobile_number: "13758949777")
+# 分销规则种子数据
+store = Store.find_by(name: "环球捕手")
+store.distribution_rules.create(name: "ANYONE", category: Settings.DISTRIBUTION_RULES.ANYONE)
