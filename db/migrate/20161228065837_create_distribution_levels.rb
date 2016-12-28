ad9af08552c5c2ad1b9061ab5f930ba7ea71bcfb
zhangwei <719048757@qq.com>
2016-12-29 00:41:31 +0800

    code

--- db/migrate/20161228065837_create_distribution_levels.rb
+++ db/migrate/20161228065837_create_distribution_levels.rb
@@ -1,6 +1,11 @@
 class CreateDistributionLevels < ActiveRecord::Migration[5.0]
   def change
     create_table :distribution_levels do |t|
+      t.float :commission_ratio
+      t.integer :level
+      t.float :minimum
+      t.float :maximum
+      t.string :remark, :limit => 256
 
       t.timestamps
     end
--- db/migrate/20161228065910_create_customer_account_details.rb
+++ db/migrate/20161228065910_create_customer_account_details.rb
@@ -1,6 +1,12 @@
 class CreateCustomerAccountDetails < ActiveRecord::Migration[5.0]
   def change
     create_table :customer_account_details do |t|
+      t.integer :customer_id
+      t.datetime :trade_time
+      t.float :expenses_receipts
+      t.float :balance
+      t.integer :category
+      t.string :remark, :limit => 256
 
       t.timestamps
     end
