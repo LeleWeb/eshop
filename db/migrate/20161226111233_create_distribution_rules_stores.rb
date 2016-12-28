b250485e813bf7ee17359b80a37212ebc544706f
zhangwei <719048757@qq.com>
2016-12-29 00:58:33 +0800

    code

--- db/migrate/20161226111233_create_distribution_rules_stores.rb
+++ db/migrate/20161226111233_create_distribution_rules_stores.rb
@@ -1,8 +1,8 @@
 class CreateDistributionRulesStores < ActiveRecord::Migration[5.0]
   def change
     create_table :distribution_rules_stores do |t|
-      t.belongs_to :distribution_rules
-      t.belongs_to :stores
+      t.belongs_to :distribution_rule
+      t.belongs_to :store
     end
   end
 end
