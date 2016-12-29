46b0d03855de09ababd4a376b1ea1e1a29d8c3ae
zhangwei <719048757@qq.com>
2016-12-29 23:22:24 +0800

    code

--- app/services/bank_accounts_service.rb
+++ app/services/bank_accounts_service.rb
@@ -1,4 +1,4 @@
-class BackAccountsService < BaseService
+class BankAccountsService < BaseService
   def create_back_account(customer, back_account_params)
     back_account = customer.back_accounts.create(back_account_params)
     CommonService.response_format(ResponseCode.COMMON.OK, back_account)
