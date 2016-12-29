890603bf2b5e8712f4ec066f46d0f8d7980b8e86
zhangwei <719048757@qq.com>
2016-12-29 18:18:20 +0800

    code

--- db/migrate/20161229083356_create_customer_accounts.rb
+++ db/migrate/20161229083356_create_customer_accounts.rb
@@ -1,7 +1,8 @@
 class CreateCustomerAccounts < ActiveRecord::Migration[5.0]
   def change
     create_table :customer_accounts do |t|
-
+      t.float :withdraw_sum
+      t.string :remark
       t.timestamps
     end
   end
--- db/migrate/20161229083423_create_withdraw_details.rb
+++ db/migrate/20161229083423_create_withdraw_details.rb
@@ -1,7 +1,12 @@
 class CreateWithdrawDetails < ActiveRecord::Migration[5.0]
   def change
     create_table :withdraw_details do |t|
-
+      t.integer :customer_id
+      t.integer :store_id
+      t.float :sum
+      t.datetime :operate_time
+      t.integer :status
+      t.string :remark
       t.timestamps
     end
   end
