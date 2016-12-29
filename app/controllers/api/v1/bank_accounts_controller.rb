554958a7479ed5ade709b468c0c6052678f3a252
zhangwei <719048757@qq.com>
2016-12-29 23:47:17 +0800

    code

--- app/controllers/api/v1/bank_accounts_controller.rb
+++ app/controllers/api/v1/bank_accounts_controller.rb
@@ -1,12 +1,37 @@
 class Api::V1::BankAccountsController < Api::V1::BaseController
-  before_action :set_customer, only: [:create]
+  before_action :set_bank_account, only: [:show, :update, :destroy]
+  before_action :set_customer, only: [:create, :index]
 
-  # POST /accounts
+  # GET /bank_accounts
+  def index
+    render json: BankAccountsService.new.get_bank_accounts(@customer)
+  end
+
+  # GET /bank_accounts/1
+  def show
+    render json: BankAccountsService.new.get_bank_account(@bank_account)
+  end
+
+  # POST /bank_accounts
   def create
     render json: BankAccountsService.new.create_bank_account(@customer, bank_account_params)
   end
 
+  # PATCH/PUT /bank_accounts/1
+  def update
+    render json: BankAccountsService.new.update_bank_account(@bank_account, bank_account_params)
+  end
+
+  # DELETE /bank_accounts /1
+  def destroy
+    render json: BankAccountsService.new.destory_bank_account(@bank_account)
+  end
+
   private
+
+  def set_bank_account
+    @bank_account = BankAccount.find(params[:id])
+  end
 
   def set_customer
     @customer = Customer.find(params[:customer_id])
--- app/services/bank_accounts_service.rb
+++ app/services/bank_accounts_service.rb
@@ -1,7 +1,45 @@
 class BankAccountsService < BaseService
-  def create_bank_account(customer, bank_account_params)
-    bank_account = customer.bank_accounts.create(bank_account_params)
+
+  def get_bank_accounts(customer)
+    CommonService.response_format(ResponseCode.COMMON.OK, customer.bank_accounts)
+  end
+
+  def get_bank_account(bank_account)
     CommonService.response_format(ResponseCode.COMMON.OK, bank_account)
+  end
+
+  def create_bank_account(customer, bank_account_params)
+    Address.transaction do
+      # 处理默认地址唯一性
+      if bank_account_params[:is_default] == true
+        customer.bank_accounts.collect{|bank_account| bank_account.update(is_default: false)}
+      end
+
+      bank_account = customer.bank_accounts.create!(bank_account_params)
+      CommonService.response_format(ResponseCode.COMMON.OK, bank_account)
+    end
+  end
+
+  def update_bank_account(bank_account, bank_account_params)
+    Address.transaction do
+      # 处理默认地址唯一性
+      if bank_account_params[:is_default] == true
+        BankAccount.where(customer_id: bank_account.customer_id).collect{|bank_account| bank_account.update(is_default: false)}
+      end
+
+      if bank_account.update!(bank_account_params)
+        CommonService.response_format(ResponseCode.COMMON.OK, bank_account)
+      else
+        ResponseCode.COMMON.FAILED['message'] = bank_account.errors
+        CommonService.response_format(ResponseCode.COMMON.FAILED)
+      end
+    end
+  end
+
+  def destory_bank_account(bank_account)
+    BankAccount.where(customer_id: bank_account.customer_id).first.update(is_default: true)
+    bank_account.destroy
+    CommonService.response_format(ResponseCode.COMMON.OK)
   end
 
 end
