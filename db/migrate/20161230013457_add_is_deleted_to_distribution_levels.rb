647c5e5b37aaf2663beaa6fcf8073780219fa188
zhangwei <719048757@qq.com>
2016-12-30 09:58:01 +0800

    code

--- db/migrate/20161230013457_add_is_deleted_to_distribution_levels.rb
+++ db/migrate/20161230013457_add_is_deleted_to_distribution_levels.rb
@@ -1,5 +1,6 @@
 class AddIsDeletedToDistributionLevels < ActiveRecord::Migration[5.0]
   def change
     add_column :distribution_levels, :is_deleted, :boolean
+    add_column :distribution_levels, :deleted_at, :datetime
   end
 end
