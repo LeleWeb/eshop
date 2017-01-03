3f507495ce0450250a09855681e70e2d90da3afa
zhangwei <719048757@qq.com>
2017-01-03 20:58:58 +0800

    code

--- app/controllers/api/v1/wxmessages_controller.rb
+++ app/controllers/api/v1/wxmessages_controller.rb
@@ -1,18 +1,19 @@
 class Api::V1::WxmessagesController < Api::V1::BaseController
-  before_action :wxmessages_params, only: [:create]
+  # before_action :wxmessages_params, only: [:create]
   skip_before_action :authenticate_user!
 
   # 微信支付结果通用通知接受接口
   def create
-    p temp = WxmessagesService.new.wxmessages_management(wxmessages_params)
+    p '9'*10,params
+    p temp = WxmessagesService.new.wxmessages_management(params["xml"])
     render xml: temp
   end
 
-  private
-
-  # Only allow a trusted parameter "white list" through.
-  def wxmessages_params
-    params.require(:xml).permit(:ToUserName, :FromUserName, :CreateTime, :MsgType, :Event)
-  end
+  # private
+  #
+  # # Only allow a trusted parameter "white list" through.
+  # def wxmessages_params
+  #   params.require(:xml).permit(:ToUserName, :FromUserName, :CreateTime, :MsgType, :Event)
+  # end
 
 end
