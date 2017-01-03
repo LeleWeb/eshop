8cf066c53d51b4c05d731911b3625bcbcccb9fe0
zhangwei <719048757@qq.com>
2017-01-03 19:26:02 +0800

    code

--- db/migrate/20170103112156_create_brands_products.rb
+++ db/migrate/20170103112156_create_brands_products.rb
@@ -1,8 +1,8 @@
 class CreateBrandsProducts < ActiveRecord::Migration[5.0]
   def change
-    create_table :brands_products do |t|
-
-      t.timestamps
+    create_table :brands_products, id: false  do |t|
+      t.belongs_to :brand
+      t.belongs_to :product
     end
   end
 end
