012d76fb4e8bea5c1194cadfe521e011f75eeb6f
zhangwei <719048757@qq.com>
2017-01-03 19:05:13 +0800

    code

--- app/controllers/api/v1/wxmessages_controller.rb
+++ app/controllers/api/v1/wxmessages_controller.rb
@@ -3,7 +3,15 @@ class Api::V1::WxmessagesController < Api::V1::BaseController
 
   # 微信支付结果通用通知接受接口
   def create
-    p 'a'*10,params
+    p temp = WxmessagesService.new.wxmessages_management(wxmessages_params)
+    render xml: temp
+  end
+
+  private
+
+  # Only allow a trusted parameter "white list" through.
+  def wxmessages_params
+    params.require(:xml).permit(:ToUserName, :FromUserName, :CreateTime, :MsgType, :Event)
   end
 
 end
---
+++ app/services/wxmessages_service.rb
@@ -0,0 +1,31 @@
+class WxmessagesService < BaseService
+  def wxmessages_management(wxmessages_params)
+    # 关注事件,发送关注自动回复文字.
+    if wxmessages_params["Event"] == Settings.WECHAT.WXMESSAGES.EVNET.SUBSCRIBE.KEY
+      p 'b'*10,wxmessages_params
+      p 'c'*10,xml = WxmessagesService.convert_hash_to_xml(wxmessages_params,
+                                                           Settings.WECHAT.EVNET.SUBSCRIBE.XML.as_json)
+      xml
+    else
+      "success"
+    end
+  end
+
+  # 微信专用xml格式转化方法
+  def self.convert_hash_to_xml(params, xml_cdata_flag)
+    root_ele = Element.new 'xml'
+    params.each do |key, value|
+      if xml_cdata_flag[key] == true
+        detail_cdata = ''
+        CData.new(value.to_json).write(detail_cdata)
+        temp = root_ele.add_element(key)
+        temp.add_text(detail_cdata)
+      else
+        temp = root_ele.add_element(key)
+        temp.add_text(value.to_s)
+      end
+    end
+    root_ele.to_s.gsub('&lt;','<').gsub('&gt','>').gsub('&quot;','"')
+  end
+
+end
