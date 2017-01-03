460e2b945cb128fd3f6ef2016d3b8bb9a75952ba
zhangwei <719048757@qq.com>
2017-01-03 19:15:47 +0800

    code

--- db/migrate/20170103110642_create_brands.rb
+++ db/migrate/20170103110642_create_brands.rb
@@ -1,7 +1,14 @@
 class CreateBrands < ActiveRecord::Migration[5.0]
   def change
     create_table :brands do |t|
-
+      t.string :name
+      t.integer :category
+      t.integer :status
+      t.string :img_url
+      t.boolean :is_recommend
+      t.string :remark
+      t.boolean :is_deleted
+      t.datetime :deleted_at
       t.timestamps
     end
   end
