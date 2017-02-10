bcaa5f6c869bcc8926db258099b9e3b755cc9355
zhangwei <719048757@qq.com>
2017-02-10 17:16:51 +0800

    code

--- db/migrate/20170210090633_create_images.rb
+++ db/migrate/20170210090633_create_images.rb
@@ -1,6 +1,13 @@
 class CreateImages < ActiveRecord::Migration[5.0]
   def change
     create_table :images do |t|
+      t.string :name, :limit => 255
+      t.integer :imageable_id
+      t.string :imageable_type, :limit => 50
+      t.string :remark, :limit => 255
+      t.boolean :is_deleted
+      t.datetime :deleted_at
+      t.timestamps
     end
   end
 end
