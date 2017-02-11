762f3241ea4f6d9e44a48c042e2ffee14cb79278
zhangwei <719048757@qq.com>
2017-02-11 20:52:27 +0800

    code

--- app/services/images_service.rb
+++ app/services/images_service.rb
@@ -8,6 +8,7 @@ class ImagesService < BaseService
     # CommonService.response_format(ResponseCode.COMMON.OK, image)
 
     image = owner.images.build(image_params)
+    p '1'*10,image_params
     if image.save
       #iterate through each of the files
       image_params[:document_data].each do |file|
@@ -21,18 +22,18 @@ class ImagesService < BaseService
     end
   end
 
-  # def upload_multiple_files(owner, image_params)
-  #   image = owner.images.build(image_params)
-  #   if image.save
-  #     #iterate through each of the files
-  #     image_params[:document_data].each do |file|
-  #       image.documents.create!(:document => file)
-  #       #create a document associated with the item that has just been created
-  #     end
-  #     CommonService.response_format(ResponseCode.COMMON.OK, image.as_json.merge(:documents => image.documents))
-  #   else
-  #     CommonService.response_format(ResponseCode.COMMON.FAILED)
-  #   end
-  # end
+  def upload_multiple_files(owner, image_params)
+    image = owner.images.build(image_params)
+    if image.save
+      #iterate through each of the files
+      image_params[:document_data].each do |file|
+        image.documents.create!(:document => file)
+        #create a document associated with the item that has just been created
+      end
+      CommonService.response_format(ResponseCode.COMMON.OK, image.as_json.merge(:documents => image.documents))
+    else
+      CommonService.response_format(ResponseCode.COMMON.FAILED)
+    end
+  end
 
 end
