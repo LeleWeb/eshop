af0f74ccbbe259d88425b23e90575a3ccd4fc64a
zhangwei <719048757@qq.com>
2017-02-11 20:41:35 +0800

    code

--- app/services/images_service.rb
+++ app/services/images_service.rb
@@ -4,22 +4,35 @@ class ImagesService < BaseService
   end
 
   def create_image(owner, image_params)
-    image = owner.images.create!(image_params)
-    CommonService.response_format(ResponseCode.COMMON.OK, image)
-  end
+    # image = owner.images.create!(image_params)
+    # CommonService.response_format(ResponseCode.COMMON.OK, image)
 
-  def upload_multiple_files(owner, image_params)
     image = owner.images.build(image_params)
     if image.save
       #iterate through each of the files
-      image_params[:document_data].each do |file|
+      image_params[:pictures].each do |file|
         image.documents.create!(:document => file)
         #create a document associated with the item that has just been created
       end
-      CommonService.response_format(ResponseCode.COMMON.OK, image.as_json.merge(:documents => image.documents))
+      CommonService.response_format(ResponseCode.COMMON.OK,
+                                    image.as_json.merge(:pictures => image.documents))
     else
       CommonService.response_format(ResponseCode.COMMON.FAILED)
     end
   end
+
+  # def upload_multiple_files(owner, image_params)
+  #   image = owner.images.build(image_params)
+  #   if image.save
+  #     #iterate through each of the files
+  #     image_params[:document_data].each do |file|
+  #       image.documents.create!(:document => file)
+  #       #create a document associated with the item that has just been created
+  #     end
+  #     CommonService.response_format(ResponseCode.COMMON.OK, image.as_json.merge(:documents => image.documents))
+  #   else
+  #     CommonService.response_format(ResponseCode.COMMON.FAILED)
+  #   end
+  # end
 
 end
