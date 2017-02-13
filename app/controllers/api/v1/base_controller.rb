1d9d1d3e96ce3b238833f177de351330d3bf19bc
zhangwei <719048757@qq.com>
2017-02-13 10:34:28 +0800

    code

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -13,7 +13,6 @@ class Api::V1::BaseController < ApplicationController
   end
 
   # API鉴权
-  # 2017.2.10 为方便调试，去掉鉴权。后续需要回复。
   before_action :authenticate_user!, except: [ :index, :show, :login ]
 
   def authenticate_user!
