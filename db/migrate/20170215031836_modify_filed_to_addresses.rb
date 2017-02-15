84673b16703f7dafd94c92cb663b27bdf9543398
zhangwei <719048757@qq.com>
2017-02-15 11:30:41 +0800

    code

--- db/migrate/20170215031836_modify_filed_to_addresses.rb
+++ db/migrate/20170215031836_modify_filed_to_addresses.rb
@@ -3,6 +3,5 @@ class ModifyFiledToAddresses < ActiveRecord::Migration[5.0]
     rename_column(:addresses, :mobile_number, :phone)
     rename_column(:addresses, :detailed_address, :address)
     add_column(:addresses, :name, :string)
-    add_column(:addresses, :is_default, :boolean, default: true)
   end
 end
