ceef973be87464e25beb865eb3423a2c0c0fafd4
zhangwei <719048757@qq.com>
2017-02-20 19:45:50 +0800

    code

--- db/migrate/20170220114208_add_display_quantity_display_unit_to_prices.rb
+++ db/migrate/20170220114208_add_display_quantity_display_unit_to_prices.rb
@@ -1,4 +1,6 @@
 class AddDisplayQuantityDisplayUnitToPrices < ActiveRecord::Migration[5.0]
   def change
+    add_column :prices, :display_quantity, :float
+    add_column :prices, :display_unit, :string
   end
 end
