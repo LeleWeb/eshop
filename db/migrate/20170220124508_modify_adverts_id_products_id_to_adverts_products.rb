a6782266798a005c6fe04c4c454406d1bb78b33f
zhangwei <719048757@qq.com>
2017-02-20 20:48:56 +0800

    code

--- db/migrate/20170220124508_modify_adverts_id_products_id_to_adverts_products.rb
+++ db/migrate/20170220124508_modify_adverts_id_products_id_to_adverts_products.rb
@@ -1,4 +1,6 @@
 class ModifyAdvertsIdProductsIdToAdvertsProducts < ActiveRecord::Migration[5.0]
   def change
+    rename_column(:adverts_products, :adverts_id, :advert_id)
+    rename_column(:adverts_products, :products_id, :product_id)
   end
 end
