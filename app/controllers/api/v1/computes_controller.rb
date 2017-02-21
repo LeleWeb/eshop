7aed73b044c9e6f6a5aa40cfe5fcabd481a5b371
zhangwei <719048757@qq.com>
2017-02-21 19:52:58 +0800

    code

--- app/controllers/api/v1/computes_controller.rb
+++ app/controllers/api/v1/computes_controller.rb
@@ -1,3 +1,13 @@
 class Api::V1::ComputesController < Api::V1::BaseController
+  # POST /api/v1/computes
+  def create
+    render json: ComputesService.new.create_compute(compute_params)
+  end
 
+  private
+  # Only allow a trusted parameter "white list" through.
+  def compute_params
+    params.require(:compute).permit(:category,
+                                    :params)
+  end
 end
