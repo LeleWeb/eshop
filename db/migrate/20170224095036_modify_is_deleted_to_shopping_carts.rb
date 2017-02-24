c1fed0d8c905b3a23686d8e12131404ed72f78bd
zhangwei <719048757@qq.com>
2017-02-24 18:03:07 +0800

    code

--- db/migrate/20170224095036_modify_is_deleted_to_shopping_carts.rb
+++ db/migrate/20170224095036_modify_is_deleted_to_shopping_carts.rb
@@ -1,5 +1,6 @@
 class ModifyIsDeletedToShoppingCarts < ActiveRecord::Migration[5.0]
   def change
-    change_column :shopping_carts, :is_deleted, :boolean, default: false, null: false
+    remove_column :shopping_carts, :is_deleted
+    add_column :shopping_carts, :is_deleted, :boolean, default: false, null: false
   end
 end
