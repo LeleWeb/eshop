5aa37dafb2ea9d52318a742292fe4fca1147d51f
zhangwei <719048757@qq.com>
2017-02-26 23:12:28 +0800

    code

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -2,4 +2,9 @@ class ShoppingCart < ApplicationRecord
   belongs_to :owner, polymorphic: true
   belongs_to :product
   belongs_to :price
+
+  # 支持团队套餐添加购物车，使用购物车项自连接解决此问题。
+  has_many :subitems, class_name: "ShoppingCart", foreign_key: "parent_id"
+
+  belongs_to :parent, class_name: "ShoppingCart"
 end
