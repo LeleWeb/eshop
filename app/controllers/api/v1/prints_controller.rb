397645dd4f9782ca8ccdf4a2ce57c52277bf8e5a
zhangwei <719048757@qq.com>
2017-02-27 11:31:32 +0800

    code

--- app/controllers/api/v1/prints_controller.rb
+++
@@ -1,2 +0,0 @@
-class Api::V1::PrintsController < ApplicationController
-end
--- db/migrate/20170227031458_create_printers.rb
+++
@@ -1,8 +0,0 @@
-class CreatePrinters < ActiveRecord::Migration[5.0]
-  def change
-    create_table :printers do |t|
-
-      t.timestamps
-    end
-  end
-end
