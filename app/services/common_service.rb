4775b7222d501a6ebd933da2a65f6f3848df9f65
zhangwei <719048757@qq.com>
2017-02-28 16:42:21 +0800

    code

--- app/services/common_service.rb
+++ app/services/common_service.rb
@@ -76,37 +76,13 @@ class CommonService < BaseService
 
   # http请求接口
   def self.post(url, params)
-    p '4'*10,url, params
     uri = URI(url)
-    p '5'*10,uri
-    req = Net::HTTP::Post.new(uri)
-    p '6'*10,req
-    req.content_type = 'application/x-www-form-urlencoded'
-    p '7'*10,req
-    req.set_form_data(params)
-    p '8'*10,req
-    res = nil
-    res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-      p '9'*10,http,req
-      res = http.request(req)
-      p '10'*10,res
+    res = Net::HTTP.post_form(uri, params)
+    if res.is_a?(Net::HTTPSuccess)
+      res.body
+    else
+      nil
     end
-    p '10'*10,res,res.body
-    res.body
-    # case res
-    #   when Net::HTTPSuccess, Net::HTTPRedirection
-    #     res.body
-    #   else
-    #     res.value
-    # end
-
-    # uri = URI(url)
-    # res = Net::HTTP.post_form(uri, params)
-    # if res.is_a?(Net::HTTPSuccess)
-    #   res.body
-    # else
-    #   nil
-    # end
   end
 
 end
