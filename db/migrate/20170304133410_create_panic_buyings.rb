2e29724614dbba15db3c1c608b1a090d08f8a002
zhangwei <719048757@qq.com>
2017-03-04 21:45:21 +0800

    code

--- db/migrate/20170304133410_create_panic_buyings.rb
+++ db/migrate/20170304133410_create_panic_buyings.rb
@@ -1,8 +1,15 @@
 class CreatePanicBuyings < ActiveRecord::Migration[5.0]
   def change
     create_table :panic_buyings do |t|
+      t.datetime :begin_time
+      t.datetime :end_time
 
       t.timestamps
+    end
+
+    create_table :panic_buyings_products, id: false do |t|
+      t.belongs_to :panic_buying, index: true
+      t.belongs_to :product, index: true
     end
   end
 end
