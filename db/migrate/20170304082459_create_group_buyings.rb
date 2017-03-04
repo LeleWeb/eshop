7fd78255bdb12a8917396900ca0d3a112c0cb70e
zhangwei <719048757@qq.com>
2017-03-04 20:44:27 +0800

    code

--- db/migrate/20170304082459_create_group_buyings.rb
+++ db/migrate/20170304082459_create_group_buyings.rb
@@ -2,15 +2,15 @@ class CreateGroupBuyings < ActiveRecord::Migration[5.0]
   def change
     create_table :group_buyings do |t|
       t.integer :product_id
-      t.integer :current_number
-      t.float :completion_rate
-      t.float :target_amount
-      t.float :current_amount
+      t.integer :current_number, default: 0
+      t.float :completion_rate, default: 0
+      t.float :target_amount, default: 0
+      t.float :current_amount, default: 0
       t.datetime :begin_time
       t.datetime :end_time
-      t.float :limit_min
-      t.float :limit_max
-      t.boolean :is_deleted
+      t.float :limit_min, default: 0
+      t.float :limit_max, default: 0
+      t.boolean :is_deleted, default: false
       t.datetime :deleted_at
       t.timestamps
     end
