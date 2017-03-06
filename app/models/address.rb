417449e8a9c93f408ef9d250679c24d41b293709
zhangwei <719048757@qq.com>
2017-03-06 17:01:42 +0800

    code

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,6 @@
 class Address < ApplicationRecord
   # 模型层数据验证
-  # validates :customer_id, :is_default, :phone, :address, :name, presence: true
+  validates :customer_id, :is_default, :phone, :address, :name, presence: true, on: :create
   validates :is_default, inclusion: { in: [true, false] }
   validates :phone, format: { with: /\d{11}/, message: "手机号码格式错误" }
 end
