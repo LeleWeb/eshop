6215975913d5c23498f73aa3a3a2618d5716cbf8
zhangwei <719048757@qq.com>
2017-03-07 23:37:30 +0800

    code

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,6 +1,6 @@
 class Price < ApplicationRecord
   # 模型层数据验证
-  validates :product_id, :price, :real_price, :unit, :is_default, :display_quantity, :display_unit, presence: true, on: :create
+  validates :product_id, :price, :real_price, :unit, :display_quantity, :display_unit, presence: true, on: :create
   validates :product_id, numericality: { only_integer: true, greater_than: 0 }
   validates :price, :real_price, :display_quantity, numericality: { greater_than: 0 }
   validates :unit, :display_unit, inclusion: { in: [Settings.PRICE.JIN,
