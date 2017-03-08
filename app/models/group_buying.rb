b46cba95e631dc939e747c6ecc607b11f32a8ade
zhangwei <719048757@qq.com>
2017-03-08 20:57:14 +0800

    code

--- app/models/group_buying.rb
+++ app/models/group_buying.rb
@@ -1,7 +1,8 @@
 class GroupBuying < ApplicationRecord
   # 模型层数据验证
   validates :product_id, :target_amount, :begin_time, :end_time, :limit_min, :limit_max, presence: true, on: :create
-  validates :product_id, :current_number, numericality: { only_integer: true, greater_than: 0 }
+  validates :product_id, numericality: { only_integer: true, greater_than: 0 }
+  validates :current_number, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
   validates :target_amount, :limit_min, :limit_max, numericality: { greater_than: 0 }
   validates :begin_time, :end_time, format: { with: /\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}/, message: "时间格式错误" }
 
