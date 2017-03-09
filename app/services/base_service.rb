6e475b2f3ed34622dd9289dd0e5af5ff9268046e
zhangwei <719048757@qq.com>
2017-03-09 11:10:43 +0800

    code

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -1,12 +1,2 @@
-class BaseService < Logger
-  # def initializer
-  #   logger = File.open("#{Rails.root}/log/eshop.log", 'a')
-  #   logger.sync = true
-  #   log = BaseService.new(logger)
-  # end
-
-  # def format_message(severity, timestamp, progname, msg)
-  #   "#{timestamp.to_formatted_s(:db)} #{severity} #{msg}\n"
-  # end
-  
+class BaseService
 end
