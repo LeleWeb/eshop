8a363986ff97b8256c48ff86f0c184f6f81800ac
zhangwei <719048757@qq.com>
2017-03-09 16:50:30 +0800

    code

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -3,10 +3,11 @@ class Setting < ApplicationRecord
 
   # 模型层数据验证
   validates :setting_type, :position, presence: true, on: :create
-  validates :setting_type, inclusion: { in: [Settings.PRODUCT_STATUS.UNDERCARRIAGE,
-                                       Settings.PRODUCT_STATUS.GROUNDING] }
-  validates :position, inclusion: { in: [Settings.PRODUCT_STATUS.UNDERCARRIAGE,
-                                       Settings.PRODUCT_STATUS.GROUNDING] }
+  validates :sort, numericality: { only_integer: true, greater_than: 0 }
+  validates :setting_type, inclusion: { in: [Settings.SETTING.HOME_PRODUCT }
+  validates :position, inclusion: { in: [Settings.PRODUCT_CATEGORY.SINGLE_SETMEAL,
+                                         Settings.PRODUCT_CATEGORY.PERSONAL_SETMEAL,
+                                         Settings.PRODUCT_CATEGORY.TEAM_SETMEAL] }
   validates :is_deleted, inclusion: { in: [true, false] }, allow_nil: true
 
 end
