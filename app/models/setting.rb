cc95cf97c7d006a95443039cd93fac7bc058cbe0
zhangwei <719048757@qq.com>
2017-03-10 10:10:37 +0800

    code

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -3,9 +3,10 @@ class Setting < ApplicationRecord
 
   # 模型层数据验证
   validates :setting_type, presence: true, on: :create
-  validates :sort, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
+  validates :sort, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
   validates :setting_type, inclusion: { in: [Settings.SETTING.HOME_PRODUCT] }
-  validates :position, inclusion: { in: [Settings.PRODUCT_CATEGORY.SINGLE_SETMEAL,
+  validates :position, inclusion: { in: [0,
+                                         Settings.PRODUCT_CATEGORY.SINGLE_SETMEAL,
                                          Settings.PRODUCT_CATEGORY.PERSONAL_SETMEAL,
                                          Settings.PRODUCT_CATEGORY.TEAM_SETMEAL] }
   validates :is_deleted, inclusion: { in: [true, false] }, allow_nil: true
