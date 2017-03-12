19ccb029277020d49a8ec22fd33b2b3c11203cec
zhangwei <719048757@qq.com>
2017-03-12 14:40:21 +0800

    code

--- db/migrate/20170312061959_add_field_to_shopping_carts.rb
+++ db/migrate/20170312061959_add_field_to_shopping_carts.rb
@@ -1,4 +1,9 @@
 class AddFieldToShoppingCarts < ActiveRecord::Migration[5.0]
   def change
+    add_column :shopping_carts, :product_img, :string, default: ""
+    add_column :shopping_carts, :product_name, :string, default: ""
+    add_column :shopping_carts, :product_desc, :string, default: ""
+    add_column :shopping_carts, :product_price, :float, default: 0
+    add_column :shopping_carts, :product_unit, :integer, default: nil
   end
 end
